7b6f7af55b7ea6aa1a554dbf3daea7328af2971a
Silas <azolyth@gmail.com>
2022-08-31 14:37:21 -0700

    Styled search input

--- index.js
+++ index.js
@@ -5,12 +5,33 @@
 const searchInput = document.getElementById('search-input');
 
 const getMovies = async (e) => {
-  e.preventDefault(); // Temporary until button handler
+  e.preventDefault();
 
   const response = await fetch(`http://www.omdbapi.com/?t=${searchInput.value}&apikey=5510309e`);
   const data = await response.json();
 
   console.log(data);
+  displayMovies(data);
 };
 
+const displayMovies = (movie) => {
+  document.getElementById('watchlist').innerHTML = `
+    <div class="movie-card" >
+        <img src=${movie.Poster}>
+        <div class="question-mark" >
+            <div class="movie-title-rating">
+                <h2 class="movie-title" >${movie.Title} <span>${movie.imdbRating}</span></h2>
+            </div>
+            <div class="movie-info" >
+                <p>${movie.Runtime}</p>
+                <p>${movie.Genre}</p>
+                <button class="watchlist-remove-btn">Watchlist</button>
+            </div>
+            <p class="movie-plot" >${movie.Plot}</p>
+        </div>
+    </div>
+    `;
+};
+
+getMovies();
 document.getElementById('search-btn').addEventListener('click', getMovies);
