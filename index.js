f0f36f878e417def6bed7d69e49934587c4d16f9
Silas <azolyth@gmail.com>
2022-08-31 15:42:35 -0700

    Typography adjustments

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const searchInput = document.getElementById('search-input');
 
 const getMovies = async (e) => {
   e.preventDefault();
+  e.target.value;
 
   const response = await fetch(`http://www.omdbapi.com/?t=${searchInput.value}&apikey=5510309e`);
   const data = await response.json();
@@ -14,24 +15,27 @@ const getMovies = async (e) => {
   displayMovies(data);
 };
 
-const displayMovies = (movie) => {
-  document.getElementById('watchlist').innerHTML = `
+const displayMovies = (movies) => {
+  let displayMoviesHtml = '';
+
+  displayMoviesHtml += `
     <div class="movie-card" >
-        <img src=${movie.Poster}>
+        <img src=${movies.Poster}>
         <div class="question-mark" >
             <div class="movie-title-rating">
-                <h2 class="movie-title" >${movie.Title} <span>${movie.imdbRating}</span></h2>
+                <h2 class="movie-title" >${movies.Title} <span class="movie-rating" >${movies.imdbRating}</span></h2>
             </div>
             <div class="movie-info" >
-                <p>${movie.Runtime}</p>
-                <p>${movie.Genre}</p>
+                <p>${movies.Runtime}</p>
+                <p>${movies.Genre}</p>
                 <button class="watchlist-remove-btn">Watchlist</button>
             </div>
-            <p class="movie-plot" >${movie.Plot}</p>
+            <p class="movie-plot" >${movies.Plot}</p>
         </div>
     </div>
     `;
+
+  document.getElementById('watchlist').innerHTML = displayMoviesHtml;
 };
 
-getMovies();
 document.getElementById('search-btn').addEventListener('click', getMovies);
